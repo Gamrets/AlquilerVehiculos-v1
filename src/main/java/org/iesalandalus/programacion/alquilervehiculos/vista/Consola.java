2cd4554e2b81dacbe37315493c172d78e0389e49
Konstantin <constantinopladwar@gmail.com>
2023-02-25 16:22:19 +0100

    10. Termine clase Consola

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/Consola.java
@@ -0,0 +1,145 @@
+package org.iesalandalus.programacion.alquilervehiculos.vista;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Turismo;
+import org.iesalandalus.programacion.utilidades.Entrada;
+
+public class Consola {
+
+	protected static final String PATRON_FECHA = "dd/LL/yyyy";
+	protected static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern(PATRON_FECHA);
+
+	private Consola() {
+
+	}
+
+	public static void mostrarCabecera(String mensaje) {
+		System.out.printf("%n%s%n", mensaje);
+		String formatoStr = "%0" + mensaje.length() + "d%n";
+		System.out.println(String.format(formatoStr, 0).replace("0", "-"));
+	}
+
+	public static void mostrarMenu() {
+		mostrarCabecera("Gestión de reservas de vehiculos");
+		System.out.println("");
+		for (Opcion opcion : Opcion.values()) {
+			System.out.println(opcion);
+		}
+	}
+
+	private static String leerCadena(String mensaje) {
+		System.out.println(mensaje);
+		String cadena = Entrada.cadena();
+		return cadena;
+	}
+
+	private static Integer leerEntero(String mensaje) {
+		System.out.println(mensaje);
+		Integer entero = Entrada.entero();
+		return entero;
+	}
+
+	private static LocalDate leerFecha(String mensaje) {
+		System.out.println(mensaje);
+		LocalDate fecha = LocalDate.parse(Entrada.cadena(), FORMATO_FECHA);
+		return fecha;
+	}
+
+	public static Opcion elegirOpcion() throws OperationNotSupportedException {
+
+		try {
+			Opcion opcion = null;
+
+			while (opcion == null) {
+				int ordinal = leerEntero("Introduce numero de opcion que quieres ejecutar: ");
+				opcion = Opcion.getOpcionSegunOrdinal(ordinal);
+			}
+			return opcion;
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
+		return null;
+	}
+
+	public static String leerNombre() {
+		String nombre = leerCadena("Introduce el nombre: ");
+		return nombre;
+	}
+
+	public static String leerTelefono() {
+		String telefono = leerCadena("Introduce el telefono: ");
+		return telefono;
+	}
+
+	public static Cliente leerCliente() {
+
+		Cliente cliente = null;
+		String nombre = leerNombre();
+		String dni = leerCadena("Introduce DNI del cliente: ");
+		String telefono = leerTelefono();
+
+		try {
+			cliente = new Cliente(nombre, dni, telefono);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return cliente;
+	}
+
+	public static Cliente leerClienteDni() {
+		// Cliente cliente;
+		String dni = leerCadena("Introduce DNI del cliente: ");
+		return Cliente.getClienteConDni(dni);
+	}
+
+	public static Turismo leerTurismo() {
+
+		Turismo turismo = null;
+		String marca = leerCadena("Introduce marca: ");
+		String modelo = leerCadena("Introduce modelo: ");
+		int cilindrada = leerEntero("Introduce numero cilindrada: ");
+		String matricula = leerCadena("Introduce matricula(1111AAA): ");
+
+		try {
+			turismo = new Turismo(marca, modelo, cilindrada, matricula);
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+		}
+		return turismo;
+	}
+
+	public static Turismo leerTurismoMatricula() {
+
+		String matricula = leerCadena("Introduce matricula(1111AAA): ");
+		return Turismo.getTurismoConMatricula(matricula);
+	}
+
+	public static Alquiler leerAlquiler() {
+
+		Cliente cliente = leerClienteDni();
+		Turismo turismo = leerTurismoMatricula();
+		Alquiler alquiler = null;
+		LocalDate fechaDate = leerFecha("Introduce fecha de alquiler(dd/mm/aaaa):");
+
+		try {
+			alquiler = new Alquiler(cliente, turismo, fechaDate);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return alquiler;
+	}
+
+	public static LocalDate leerFechaDevolucion() {
+		LocalDate fechaDevolucion = leerFecha("Introduce fecha de devolucion(dd/mm/aaaa): ");
+		return fechaDevolucion;
+	}
+
+}
